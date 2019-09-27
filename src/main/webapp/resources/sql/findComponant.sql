96d6a68e0d97e4cf47726cf8a7dd97c8de112049
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2019-09-27 11:46:16 +0530

    Add Values Multiple Times

--- src/main/webapp/resources/sql/findComponant.sql
+++ src/main/webapp/resources/sql/findComponant.sql
@@ -1,2 +1,2 @@
-select id, `NAME`, `ITEM_ID`, `ITEMVALUE_ID`, `PARENTCOMPONENT_ID` from component where `id`=18543;
+select id, `NAME`, `ITEM_ID`, `ITEMVALUE_ID`, `PARENTCOMPONENT_ID` from component where `id`=18677;
 select id,`NAME`,code from item where id=17001 or id=17002;
