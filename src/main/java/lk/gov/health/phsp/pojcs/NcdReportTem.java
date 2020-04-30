3606a59ac26d0e178ede07986d62d595787d19de
chims <chims@hims-development>
2020-04-30 01:26:47 +0000

    Temporary Development of NCD Report

---
+++ src/main/java/lk/gov/health/phsp/pojcs/NcdReportTem.java
@@ -0,0 +1,488 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 chims.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package lk.gov.health.phsp.pojcs;
+
+/**
+ *
+ * @author chims
+ */
+public class NcdReportTem {
+
+    
+    
+    private double TotalNoOfParticipants;
+    private double Age20To34;
+    private double Age35To65;
+    private double AgeGt65;
+    private double NumberWithAlreadyDiagnosedDiseasesDM;
+    private double NumberWithAlreadyDiagnosedDiseasesHypertension;
+    private double NumberWithAlreadyDiagnosedDiseasesCVA;
+    private double NumberWithAlreadyDiagnosedDiseasesHeartDiseases;
+    private double NumberWithAlreadyDiagnosedDiseasesCKD;
+    private double NumberWithAlreadyDiagnosedDiseasesCancer;
+    private double NumberWithAlreadyDiagnosedDiseasesOther;
+    private double PhysicalActivitySedentary;
+    private double PhysicalActivityNonSedentary;
+    private double RiskFactorsNoOfSmokers;
+    private double RiskFactorsNoOfChewingTobacco;
+    private double RiskFactorsNoOfOtherSmokelessTobaccoUsers;
+    private double NoOfAlcoholUsers;
+    private double BmiLt18_5;
+    private double Bmi18_5to22_9;
+    private double Bmi23to24_9;
+    private double Bmi25to29_9;
+    private double BmiGt30;
+    private double WaistCircumstanceMaleGt90cmOrFemaleGt80cm;
+    private double Sbp120andDbp80;
+    private double Sbo120To139AndDbp80To89;
+    private double Sbp140AndDbp90OrAbove;
+    private double NoOfParticipantsWhoHaveUndergoneTheOralExamination;
+    private double NoOfParticipantsWhoHaveUndergoneTheBreastExaminatioin;
+    private double NoOfParticipantsWhoHaveUndergoneTheTyroidExamination;
+    private double NoOfParticipantsWhoHaveUndergoneThePapSmearExamination;
+    private double RbsLt140;
+    private double Rbs140To199;
+    private double RbsGte200;
+    private double FbsLt100;
+    private double Fbs100To125;
+    private double FbsGt126;
+    private double SerumCreatininNormal;
+    private double SerumCreatininHigh;
+    private double SerumCholosterolMgDlLt270;
+    private double SerumCholosterolMgDlLt270To299;
+    private double SerumCholosterolMgDlLtGt300;
+    private double CvsRiskLt10;
+    private double CvsRisk10to20;
+    private double CvsRisk20to30;
+    private double CvsRiskGt30;
+    private double ReferralMedicalClinicOfTheInstitution;
+    private double ReferralSpecialMedicalClinic;
+    private double ReferralHealthyLifestyleCenter;
+    private double ReferralWellWomanClinic;
+    private double ReferralDentalClinic;
+
+    public double getTotalNoOfParticipants() {
+        return TotalNoOfParticipants;
+    }
+
+    public void setTotalNoOfParticipants(double TotalNoOfParticipants) {
+        this.TotalNoOfParticipants = TotalNoOfParticipants;
+    }
+
+    public double getAge20To34() {
+        return Age20To34;
+    }
+
+    public void setAge20To34(double Age20To34) {
+        this.Age20To34 = Age20To34;
+    }
+
+    public double getAge35To65() {
+        return Age35To65;
+    }
+
+    public void setAge35To65(double Age35To65) {
+        this.Age35To65 = Age35To65;
+    }
+
+    public double getAgeGt65() {
+        return AgeGt65;
+    }
+
+    public void setAgeGt65(double AgeGt65) {
+        this.AgeGt65 = AgeGt65;
+    }
+
+    public double getNumberWithAlreadyDiagnosedDiseasesDM() {
+        return NumberWithAlreadyDiagnosedDiseasesDM;
+    }
+
+    public void setNumberWithAlreadyDiagnosedDiseasesDM(double NumberWithAlreadyDiagnosedDiseasesDM) {
+        this.NumberWithAlreadyDiagnosedDiseasesDM = NumberWithAlreadyDiagnosedDiseasesDM;
+    }
+
+    public double getNumberWithAlreadyDiagnosedDiseasesHypertension() {
+        return NumberWithAlreadyDiagnosedDiseasesHypertension;
+    }
+
+    public void setNumberWithAlreadyDiagnosedDiseasesHypertension(double NumberWithAlreadyDiagnosedDiseasesHypertension) {
+        this.NumberWithAlreadyDiagnosedDiseasesHypertension = NumberWithAlreadyDiagnosedDiseasesHypertension;
+    }
+
+    public double getNumberWithAlreadyDiagnosedDiseasesCVA() {
+        return NumberWithAlreadyDiagnosedDiseasesCVA;
+    }
+
+    public void setNumberWithAlreadyDiagnosedDiseasesCVA(double NumberWithAlreadyDiagnosedDiseasesCVA) {
+        this.NumberWithAlreadyDiagnosedDiseasesCVA = NumberWithAlreadyDiagnosedDiseasesCVA;
+    }
+
+    public double getNumberWithAlreadyDiagnosedDiseasesHeartDiseases() {
+        return NumberWithAlreadyDiagnosedDiseasesHeartDiseases;
+    }
+
+    public void setNumberWithAlreadyDiagnosedDiseasesHeartDiseases(double NumberWithAlreadyDiagnosedDiseasesHeartDiseases) {
+        this.NumberWithAlreadyDiagnosedDiseasesHeartDiseases = NumberWithAlreadyDiagnosedDiseasesHeartDiseases;
+    }
+
+    public double getNumberWithAlreadyDiagnosedDiseasesCKD() {
+        return NumberWithAlreadyDiagnosedDiseasesCKD;
+    }
+
+    public void setNumberWithAlreadyDiagnosedDiseasesCKD(double NumberWithAlreadyDiagnosedDiseasesCKD) {
+        this.NumberWithAlreadyDiagnosedDiseasesCKD = NumberWithAlreadyDiagnosedDiseasesCKD;
+    }
+
+    public double getNumberWithAlreadyDiagnosedDiseasesCancer() {
+        return NumberWithAlreadyDiagnosedDiseasesCancer;
+    }
+
+    public void setNumberWithAlreadyDiagnosedDiseasesCancer(double NumberWithAlreadyDiagnosedDiseasesCancer) {
+        this.NumberWithAlreadyDiagnosedDiseasesCancer = NumberWithAlreadyDiagnosedDiseasesCancer;
+    }
+
+    public double getNumberWithAlreadyDiagnosedDiseasesOther() {
+        return NumberWithAlreadyDiagnosedDiseasesOther;
+    }
+
+    public void setNumberWithAlreadyDiagnosedDiseasesOther(double NumberWithAlreadyDiagnosedDiseasesOther) {
+        this.NumberWithAlreadyDiagnosedDiseasesOther = NumberWithAlreadyDiagnosedDiseasesOther;
+    }
+
+    public double getPhysicalActivitySedentary() {
+        return PhysicalActivitySedentary;
+    }
+
+    public void setPhysicalActivitySedentary(double PhysicalActivitySedentary) {
+        this.PhysicalActivitySedentary = PhysicalActivitySedentary;
+    }
+
+    public double getPhysicalActivityNonSedentary() {
+        return PhysicalActivityNonSedentary;
+    }
+
+    public void setPhysicalActivityNonSedentary(double PhysicalActivityNonSedentary) {
+        this.PhysicalActivityNonSedentary = PhysicalActivityNonSedentary;
+    }
+
+    public double getRiskFactorsNoOfSmokers() {
+        return RiskFactorsNoOfSmokers;
+    }
+
+    public void setRiskFactorsNoOfSmokers(double RiskFactorsNoOfSmokers) {
+        this.RiskFactorsNoOfSmokers = RiskFactorsNoOfSmokers;
+    }
+
+    public double getRiskFactorsNoOfChewingTobacco() {
+        return RiskFactorsNoOfChewingTobacco;
+    }
+
+    public void setRiskFactorsNoOfChewingTobacco(double RiskFactorsNoOfChewingTobacco) {
+        this.RiskFactorsNoOfChewingTobacco = RiskFactorsNoOfChewingTobacco;
+    }
+
+    public double getRiskFactorsNoOfOtherSmokelessTobaccoUsers() {
+        return RiskFactorsNoOfOtherSmokelessTobaccoUsers;
+    }
+
+    public void setRiskFactorsNoOfOtherSmokelessTobaccoUsers(double RiskFactorsNoOfOtherSmokelessTobaccoUsers) {
+        this.RiskFactorsNoOfOtherSmokelessTobaccoUsers = RiskFactorsNoOfOtherSmokelessTobaccoUsers;
+    }
+
+    public double getNoOfAlcoholUsers() {
+        return NoOfAlcoholUsers;
+    }
+
+    public void setNoOfAlcoholUsers(double NoOfAlcoholUsers) {
+        this.NoOfAlcoholUsers = NoOfAlcoholUsers;
+    }
+
+    public double getBmiLt18_5() {
+        return BmiLt18_5;
+    }
+
+    public void setBmiLt18_5(double BmiLt18_5) {
+        this.BmiLt18_5 = BmiLt18_5;
+    }
+
+    public double getBmi18_5to22_9() {
+        return Bmi18_5to22_9;
+    }
+
+    public void setBmi18_5to22_9(double Bmi18_5to22_9) {
+        this.Bmi18_5to22_9 = Bmi18_5to22_9;
+    }
+
+    public double getBmi23to24_9() {
+        return Bmi23to24_9;
+    }
+
+    public void setBmi23to24_9(double Bmi23to24_9) {
+        this.Bmi23to24_9 = Bmi23to24_9;
+    }
+
+    public double getBmi25to29_9() {
+        return Bmi25to29_9;
+    }
+
+    public void setBmi25to29_9(double Bmi25to29_9) {
+        this.Bmi25to29_9 = Bmi25to29_9;
+    }
+
+    public double getBmiGt30() {
+        return BmiGt30;
+    }
+
+    public void setBmiGt30(double BmiGt30) {
+        this.BmiGt30 = BmiGt30;
+    }
+
+    public double getWaistCircumstanceMaleGt90cmOrFemaleGt80cm() {
+        return WaistCircumstanceMaleGt90cmOrFemaleGt80cm;
+    }
+
+    public void setWaistCircumstanceMaleGt90cmOrFemaleGt80cm(double WaistCircumstanceMaleGt90cmOrFemaleGt80cm) {
+        this.WaistCircumstanceMaleGt90cmOrFemaleGt80cm = WaistCircumstanceMaleGt90cmOrFemaleGt80cm;
+    }
+
+    public double getSbp120andDbp80() {
+        return Sbp120andDbp80;
+    }
+
+    public void setSbp120andDbp80(double Sbp120andDbp80) {
+        this.Sbp120andDbp80 = Sbp120andDbp80;
+    }
+
+    public double getSbo120To139AndDbp80To89() {
+        return Sbo120To139AndDbp80To89;
+    }
+
+    public void setSbo120To139AndDbp80To89(double Sbo120To139AndDbp80To89) {
+        this.Sbo120To139AndDbp80To89 = Sbo120To139AndDbp80To89;
+    }
+
+    public double getSbp140AndDbp90OrAbove() {
+        return Sbp140AndDbp90OrAbove;
+    }
+
+    public void setSbp140AndDbp90OrAbove(double Sbp140AndDbp90OrAbove) {
+        this.Sbp140AndDbp90OrAbove = Sbp140AndDbp90OrAbove;
+    }
+
+    public double getNoOfParticipantsWhoHaveUndergoneTheOralExamination() {
+        return NoOfParticipantsWhoHaveUndergoneTheOralExamination;
+    }
+
+    public void setNoOfParticipantsWhoHaveUndergoneTheOralExamination(double NoOfParticipantsWhoHaveUndergoneTheOralExamination) {
+        this.NoOfParticipantsWhoHaveUndergoneTheOralExamination = NoOfParticipantsWhoHaveUndergoneTheOralExamination;
+    }
+
+    public double getNoOfParticipantsWhoHaveUndergoneTheBreastExaminatioin() {
+        return NoOfParticipantsWhoHaveUndergoneTheBreastExaminatioin;
+    }
+
+    public void setNoOfParticipantsWhoHaveUndergoneTheBreastExaminatioin(double NoOfParticipantsWhoHaveUndergoneTheBreastExaminatioin) {
+        this.NoOfParticipantsWhoHaveUndergoneTheBreastExaminatioin = NoOfParticipantsWhoHaveUndergoneTheBreastExaminatioin;
+    }
+
+    public double getNoOfParticipantsWhoHaveUndergoneTheTyroidExamination() {
+        return NoOfParticipantsWhoHaveUndergoneTheTyroidExamination;
+    }
+
+    public void setNoOfParticipantsWhoHaveUndergoneTheTyroidExamination(double NoOfParticipantsWhoHaveUndergoneTheTyroidExamination) {
+        this.NoOfParticipantsWhoHaveUndergoneTheTyroidExamination = NoOfParticipantsWhoHaveUndergoneTheTyroidExamination;
+    }
+
+    public double getNoOfParticipantsWhoHaveUndergoneThePapSmearExamination() {
+        return NoOfParticipantsWhoHaveUndergoneThePapSmearExamination;
+    }
+
+    public void setNoOfParticipantsWhoHaveUndergoneThePapSmearExamination(double NoOfParticipantsWhoHaveUndergoneThePapSmearExamination) {
+        this.NoOfParticipantsWhoHaveUndergoneThePapSmearExamination = NoOfParticipantsWhoHaveUndergoneThePapSmearExamination;
+    }
+
+    public double getRbsLt140() {
+        return RbsLt140;
+    }
+
+    public void setRbsLt140(double RbsLt140) {
+        this.RbsLt140 = RbsLt140;
+    }
+
+    public double getRbs140To199() {
+        return Rbs140To199;
+    }
+
+    public void setRbs140To199(double Rbs140To199) {
+        this.Rbs140To199 = Rbs140To199;
+    }
+
+    public double getRbsGte200() {
+        return RbsGte200;
+    }
+
+    public void setRbsGte200(double RbsGte200) {
+        this.RbsGte200 = RbsGte200;
+    }
+
+    public double getFbsLt100() {
+        return FbsLt100;
+    }
+
+    public void setFbsLt100(double FbsLt100) {
+        this.FbsLt100 = FbsLt100;
+    }
+
+    public double getFbs100To125() {
+        return Fbs100To125;
+    }
+
+    public void setFbs100To125(double Fbs100To125) {
+        this.Fbs100To125 = Fbs100To125;
+    }
+
+    public double getFbsGt126() {
+        return FbsGt126;
+    }
+
+    public void setFbsGt126(double FbsGt126) {
+        this.FbsGt126 = FbsGt126;
+    }
+
+    public double getSerumCreatininNormal() {
+        return SerumCreatininNormal;
+    }
+
+    public void setSerumCreatininNormal(double SerumCreatininNormal) {
+        this.SerumCreatininNormal = SerumCreatininNormal;
+    }
+
+    public double getSerumCreatininHigh() {
+        return SerumCreatininHigh;
+    }
+
+    public void setSerumCreatininHigh(double SerumCreatininHigh) {
+        this.SerumCreatininHigh = SerumCreatininHigh;
+    }
+
+    public double getSerumCholosterolMgDlLt270() {
+        return SerumCholosterolMgDlLt270;
+    }
+
+    public void setSerumCholosterolMgDlLt270(double SerumCholosterolMgDlLt270) {
+        this.SerumCholosterolMgDlLt270 = SerumCholosterolMgDlLt270;
+    }
+
+    public double getSerumCholosterolMgDlLt270To299() {
+        return SerumCholosterolMgDlLt270To299;
+    }
+
+    public void setSerumCholosterolMgDlLt270To299(double SerumCholosterolMgDlLt270To299) {
+        this.SerumCholosterolMgDlLt270To299 = SerumCholosterolMgDlLt270To299;
+    }
+
+    public double getSerumCholosterolMgDlLtGt300() {
+        return SerumCholosterolMgDlLtGt300;
+    }
+
+    public void setSerumCholosterolMgDlLtGt300(double SerumCholosterolMgDlLtGt300) {
+        this.SerumCholosterolMgDlLtGt300 = SerumCholosterolMgDlLtGt300;
+    }
+
+    public double getCvsRiskLt10() {
+        return CvsRiskLt10;
+    }
+
+    public void setCvsRiskLt10(double CvsRiskLt10) {
+        this.CvsRiskLt10 = CvsRiskLt10;
+    }
+
+    public double getCvsRisk10to20() {
+        return CvsRisk10to20;
+    }
+
+    public void setCvsRisk10to20(double CvsRisk10to20) {
+        this.CvsRisk10to20 = CvsRisk10to20;
+    }
+
+    public double getCvsRisk20to30() {
+        return CvsRisk20to30;
+    }
+
+    public void setCvsRisk20to30(double CvsRisk20to30) {
+        this.CvsRisk20to30 = CvsRisk20to30;
+    }
+
+    public double getCvsRiskGt30() {
+        return CvsRiskGt30;
+    }
+
+    public void setCvsRiskGt30(double CvsRiskGt30) {
+        this.CvsRiskGt30 = CvsRiskGt30;
+    }
+
+    public double getReferralMedicalClinicOfTheInstitution() {
+        return ReferralMedicalClinicOfTheInstitution;
+    }
+
+    public void setReferralMedicalClinicOfTheInstitution(double ReferralMedicalClinicOfTheInstitution) {
+        this.ReferralMedicalClinicOfTheInstitution = ReferralMedicalClinicOfTheInstitution;
+    }
+
+    public double getReferralSpecialMedicalClinic() {
+        return ReferralSpecialMedicalClinic;
+    }
+
+    public void setReferralSpecialMedicalClinic(double ReferralSpecialMedicalClinic) {
+        this.ReferralSpecialMedicalClinic = ReferralSpecialMedicalClinic;
+    }
+
+    public double getReferralHealthyLifestyleCenter() {
+        return ReferralHealthyLifestyleCenter;
+    }
+
+    public void setReferralHealthyLifestyleCenter(double ReferralHealthyLifestyleCenter) {
+        this.ReferralHealthyLifestyleCenter = ReferralHealthyLifestyleCenter;
+    }
+
+    public double getReferralWellWomanClinic() {
+        return ReferralWellWomanClinic;
+    }
+
+    public void setReferralWellWomanClinic(double ReferralWellWomanClinic) {
+        this.ReferralWellWomanClinic = ReferralWellWomanClinic;
+    }
+
+    public double getReferralDentalClinic() {
+        return ReferralDentalClinic;
+    }
+
+    public void setReferralDentalClinic(double ReferralDentalClinic) {
+        this.ReferralDentalClinic = ReferralDentalClinic;
+    }
+
+    
+    
+    
+}
