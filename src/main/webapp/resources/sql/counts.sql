0419122ff618a057f569a3e9795d059702e5b940
buddhika <buddhika@chims>
2020-10-15 03:23:18 +0530

    Before attempting change in queries

---
+++ src/main/webapp/resources/sql/counts.sql
@@ -0,0 +1,6 @@
+select count(*) from component;
+select count(*) from encounter;
+select count(*) from institution;
+select count(*) from area;
+select count(*) from client;
+select count(*) from person;
