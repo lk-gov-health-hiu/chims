c434723906cf2ea9776dc78f9aa1057d87a5d2d5
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2021-03-15 12:39:50 +0530

    SQL improved

--- src/main/webapp/resources/sql/Encounter.sql
+++ src/main/webapp/resources/sql/Encounter.sql
@@ -1,3 +1,10 @@
-select `COMPLETED`, `CREATEDAT`, `ENCOUNTERTYPE` , `ENCOUNTERDATE`, `CLIENT_ID`
+select  encounter.`CREATEDAT`, encounter.`ENCOUNTERTYPE` , encounter.`ENCOUNTERDATE`, encounter.`COMPLETED`,
+institution.`NAME`, district.`NAME`, province.`NAME`
 from encounter
-where `ID`=14203502;
+join institution on encounter.`INSTITUTION_ID` = institution.`ID`
+join client on encounter.`CLIENT_ID`=client.`ID`
+join area province on institution.`PROVINCE_ID` = province.`ID` 
+join area district on institution.`DISTRICT_ID`=district.`ID`
+join person on client.`PERSON_ID`=person.`ID`
+join item sex on person.`SEX_ID` = sex.`ID`
+;
---
+++ src/main/webapp/resources/sql/Encounter_grouped.sql
@@ -0,0 +1,3 @@
+select `COMPLETED`, `CREATEDAT`, `ENCOUNTERTYPE` , `ENCOUNTERDATE`, `CLIENT_ID`
+from encounter
+where `ID`=14203502;
