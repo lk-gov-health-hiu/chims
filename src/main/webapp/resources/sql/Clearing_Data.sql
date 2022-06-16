5b9ece5690df8a6c5f7a4c12bd0e6c337ce84e4f
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2022-06-16 12:55:53 +0530

    Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/webapp/resources/sql/Clearing_Data.sql
+++ src/main/webapp/resources/sql/Clearing_Data.sql
@@ -9,10 +9,8 @@ drop TABLE INDIVIDUALQUERYRESULT;
 drop table INDIVIDUALQUERYRESULT;
 drop table PHN;
 drop table USERTRANSACTION;
-delete from COMPONENT where DTYPE in 'ClientEncounterComponentForm' ,
-'ClientEncounterComponentFormSet',
-'ClientEncounterComponentItem';
-delete from person where id > 2;
+delete from COMPONENT where DTYPE in ('ClientEncounterComponentForm','ClientEncounterComponentFormSet','ClientEncounterComponentItem');
+delete from PERSON where id > 2;
 delete from USERAREA where WEBUSER_ID > 101;
 DELETE from USERPRIVILEGE where WEBUSER_ID > 101;
 delete from WEBUSER where ID > 101;
