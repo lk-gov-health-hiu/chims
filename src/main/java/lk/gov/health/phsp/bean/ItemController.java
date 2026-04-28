5798098fc28b286553fb6eb2e52a60fab0f6bc87
Chaturanga Manchanayake <manchitmr@gmail.com>
2026-04-28 09:38:23 +0000

    Update user titles list and order as requested

--- src/main/java/lk/gov/health/phsp/bean/ItemController.java
+++ src/main/java/lk/gov/health/phsp/bean/ItemController.java
@@ -1045,17 +1045,17 @@ public class ItemController implements Serializable {
                 + "Dictionary_Item:male_title:Mr:mr:0" + System.lineSeparator()
                 + "Dictionary_Item:female_title:Mrs:mrs:1" + System.lineSeparator()
                 + "Dictionary_Item:female_title:Miss:miss:2" + System.lineSeparator()
-                + "Dictionary_Item:male_title:Master:master:3" + System.lineSeparator()
-                + "Dictionary_Item:male_or_female_title:Baby:baby:4" + System.lineSeparator()
-                + "Dictionary_Item:male_or_female_title:Rev:rev:5" + System.lineSeparator()
-                + "Dictionary_Item:female_title:Ms:ms:6" + System.lineSeparator()
-                + "Dictionary_Item:male_or_female_title:Dr:dr:7" + System.lineSeparator()
-                + "Dictionary_Item:female_title:Dr(Mrs):drmrs:8" + System.lineSeparator()
-                + "Dictionary_Item:female_title:Dr(Miss):drmiss:9" + System.lineSeparator()
-                + "Dictionary_Item:female_title:Dr(Ms):drms:10" + System.lineSeparator()
-                + "Dictionary_Item:male_or_female_title:Rt Rev:rtrev:11" + System.lineSeparator()
-                + "Dictionary_Item:male_or_female_title:Baby of:baby_of:12" + System.lineSeparator()
-                + "Dictionary_Item:male_or_female_title:Other:title_other:13" + System.lineSeparator();
+                + "Dictionary_Item:male_or_female_title:Baby:baby:3" + System.lineSeparator()
+                + "Dictionary_Item:male_or_female_title:Baby of:baby_of:4" + System.lineSeparator()
+                + "Dictionary_Item:male_or_female_title:Dr:dr:5" + System.lineSeparator()
+                + "Dictionary_Item:female_title:Dr(Miss):drmiss:6" + System.lineSeparator()
+                + "Dictionary_Item:female_title:Dr(Mrs):drmrs:7" + System.lineSeparator()
+                + "Dictionary_Item:female_title:Dr(Ms):drms:8" + System.lineSeparator()
+                + "Dictionary_Item:male_title:Master:master:9" + System.lineSeparator()
+                + "Dictionary_Item:female_title:Ms:ms:10" + System.lineSeparator()
+                + "Dictionary_Item:male_or_female_title:Other:title_other:11" + System.lineSeparator()
+                + "Dictionary_Item:male_or_female_title:Rev:rev:12" + System.lineSeparator()
+                + "Dictionary_Item:male_or_female_title:Rt Rev:rtrev:13" + System.lineSeparator();
         addInitialMetadata(initialData);
     }
 
